96e5b877ee26a3f0a5de077b52ce4ecc6ddcc438
Elie Oko <elieoko100@gmail.com>
2023-04-17 10:22:44 +0100

    feat:build components

---
+++ components/pages/team.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="bg-blue-600 box-border w-56">
+            {{lastName}}
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name:"team",
+        props:["lastName"]
+    }
+</script>
