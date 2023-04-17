3d230e146acaa0a0f967842a233377dc16b1af1a
Elie Oko <elieoko100@gmail.com>
2023-04-17 01:07:55 +0100

    feat:config project nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    alias:{
+        "@":resolve(__dirname,"/"),
+    },
+    typescript: {
+        strict: true
+      },
+
+    css: [
+        '~/assets/main.css',
+      ],
+    postcss: {
+          plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+          },
+      },
+})
