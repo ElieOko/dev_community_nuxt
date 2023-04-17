431d2cb58a43ea7e03c75332d24c8f9573845021
Elie Oko <elieoko100@gmail.com>
2023-04-17 10:24:13 +0100

    feat:build page

---
+++ pages/project/[id].vue
@@ -0,0 +1,12 @@
+<script setup>
+const route = useRoute()
+// const $route = useNuxtApp()
+// console.log("route =>",$route);
+</script>
+
+<template>
+    <div>
+        <h2> Hi {{ route.params.id }}</h2>
+    </div>
+</template>
+
---
+++ pages/team/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Team</h2>
+        <team v-for="team_list in team_odc" :lastName="team_list.lastName"/>
+    </div>
+</template>
+
+<script>
+import { data_team } from '~/composable/lib/data';
+import team from "~/components/pages/team.vue"
+export default {
+    components:{team},
+    data(){
+        return {
+            team_odc:data_team
+        }
+    }
+}
+</script>
